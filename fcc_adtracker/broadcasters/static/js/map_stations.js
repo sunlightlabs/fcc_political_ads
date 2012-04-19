f1dce6aa459eecd38352f59fc2950f28556f5232
Daniel Cloud <dcloud@sunlightfoundation.com>
2012-04-19 17:52:14 -0400

    Refactor some map code, add custom markers.

--- fcc_adtracker/broadcasters/static/js/map_stations.js
+++ fcc_adtracker/broadcasters/static/js/map_stations.js
@@ -1,11 +1,13 @@
-const amile = 1609.344;
-
 var SLL = {
     geocoder: null,
     map: null,
     markers: null,
     searchLoc: null,
-    nearby: null
+    nearby: null,
+    list_elem: null,
+    amile: function() { return 1609.344; },
+    userMarkerImage: null,
+    stationMarkerImage: null
 };
 
 jQuery(document).ready(function() {
@@ -19,51 +21,75 @@ jQuery(document).ready(function() {
     SLL.geocoder = new gm.Geocoder();
     SLL.map = new gm.Map(document.getElementById("map_canvas"), myOptions);
     SLL.markers = {};
+    SLL.userMarkerImage = new gm.MarkerImage(userMarker_url, gm.Size(19,32));
+    SLL.stationMarkerImage = new gm.MarkerImage(stationMarker_url, gm.Size(19,32));
     
-    var locations_list = $("ul#locations");
+    SLL.list_elem = $("ul#locations");
     
     SLL.removeStationMarkers = function () {
         for each( mkr in SLL.markers) { mkr.setMap(); }
         SLL.markers = {};
     }
     
-    SLL.attachInfoWindow = function (marker, content) {
-        var infowindow = new gm.InfoWindow({
-            content: content
-        });
-        gm.event.addListener(marker, 'click', function() {
-            infowindow.open(marker.get('map'), marker);
-        });
+    SLL.attachInfoWindow = function (marker, content, open) {
+        var infowindow = new gm.InfoWindow({ content: content });
+        gm.event.addListener(marker, 'click', function() { infowindow.open(marker.get('map'), marker); });
+        if (open) { infowindow.open(marker.get('map'), marker); };
     }
+    
+    SLL.addMarker = function(id, pos, infoContent, markerImage) {
+        var marker = new gm.Marker({
+            position:  new gm.LatLng(pos[1], pos[0]),
+            draggable: false,
+            icon: markerImage
+        });
+        SLL.markers[id] = marker;
+        marker.setTitle(id);
+        SLL.attachInfoWindow(marker, infoContent);
+        return marker;
+    };
+        
+    SLL.revealList = function() {
+        console.log('revealList');
+        var li_els = SLL.list_elem.children('li');
+        $(li_els).each(function(index, element) {
+            $(element).delay(index*100).fadeIn('slow');
+        });
+    };
+    
+    SLL.revealMarkers = function(callback) {
+        for each( mkr in SLL.markers) { 
+            mkr.setMap(SLL.map);
+        }
+        callback();
+    };
     
     SLL.updateMapApp = function (location_list) {
         SLL.removeStationMarkers();
-        locations_list.empty();
+        SLL.list_elem.empty();
         var mapBounds = new gm.LatLngBounds();
         for (var i=0; i < location_list.length; i++) {
             var element = location_list[i];
             var pos = (element['addresses']['studio'] && element['addresses']['studio']['pos']) ? element['addresses']['studio']['pos'] : null;
             if (pos !== null) {
-                var marker = new gm.Marker({
-                    map: SLL.map,
-                    position:  new gm.LatLng(pos[1], pos[0])
-                });
-                SLL.markers[element.callsign] = marker;
-                marker.setTitle(element.callsign);
-                SLL.attachInfoWindow(marker, element.html);
+                var marker= SLL.addMarker(element.callsign, pos, element.html, SLL.stationMarkerImage);
                 mapBounds.extend(marker.getPosition());
             };
             var elem = $('<li>' + element.html + '</li>');
             if (element.distance) {
                 elem.append('<p class="distance">' + element.distance + ' miles (approx.)</p>');
             }
-            locations_list.append(elem);
+            elem.hide();
+            SLL.list_elem.append(elem);
+            // elem.delay(i*100).fadeIn('slow');
         };
+        gm.event.addListenerOnce(SLL.map, 'bounds_changed', function() {
+            SLL.revealMarkers(SLL.revealList);    
+        });
         SLL.map.fitBounds(mapBounds);
     }
     
     SLL.handleNearbySuccess = function(data, textStatus) {
-        console.log(textStatus);
         nearby = data;
         SLL.updateMapApp(nearby);
     }
@@ -91,8 +117,11 @@ jQuery(document).ready(function() {
                 SLL.map.setCenter(SLL.searchLoc);
                 var marker = new gm.Marker({
                     map: SLL.map,
-                    position: SLL.searchLoc
+                    position: SLL.searchLoc,
+                    icon: SLL.userMarkerImage,
+                    animation: gm.Animation.DROP
                 });
+                SLL.attachInfoWindow(marker, address);
                 marker.setTitle(address);
                 var radius = $("form#map_form select#radius").val();
                 
@@ -103,47 +132,13 @@ jQuery(document).ready(function() {
                     success: SLL.handleNearbySuccess,
                     complete: SLL.handleNearbyComplete
                 });
-                
-                                
-                // dist_service.getDistanceMatrix(
-                //   {
-                //     origins: [searchLoc],
-                //     destinations: dest_sets[0],
-                //     travelMode: google.maps.TravelMode.DRIVING,
-                //     avoidHighways: false,
-                //     avoidTolls: false
-                //   }, dist_callback);
-                
-            } else {
+
+             } else {
                 if(console) console.log("Geocode was not successful for the following reason: " + status);
             }
         });
         
         return false;
     });
-    
-/*
-    dist_service = new gm.DistanceMatrixService();
-    function dist_callback(response, status) {
-        if (status == gm.DistanceMatrixStatus.OK) {
-            var origins = response.originAddresses;
-            var destinations = response.destinationAddresses;
 
-            for (var i = 0; i < origins.length; i++) {
-                var results = response.rows[i].elements;
-                for (var j = 0; j < results.length; j++) {
-                    var element = results[j];
-                    var distance = element.distance.text;
-                    var duration = element.duration.text;
-                    var from = origins[i];
-                    var to = destinations[j];
-                    dist_results.push(element);
-                    if (console) {
-                        console.log(to + ' ' + element.distance.value/amile + ' miles: ' + duration);
-                    };
-                }
-            }
-        }
-    }
-*/
 });
