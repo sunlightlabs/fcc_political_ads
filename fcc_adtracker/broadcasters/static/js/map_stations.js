e7a1013f478c923276e377b2bb079d2d8787306f
Daniel Cloud <dcloud@sunlightfoundation.com>
2012-05-07 17:47:52 -0400

    Track current infowindow in map and keep only one open at a time. Basic chooser for multiple results from address geocoder. Bug fixes for IE (8).

--- fcc_adtracker/broadcasters/static/js/map_stations.js
+++ fcc_adtracker/broadcasters/static/js/map_stations.js
@@ -3,12 +3,14 @@ var SLF = {
     map: null,
     markers: null,
     locations: null,
-    searchLoc: null,
+    selectedGeocoderResult: null,
+    userMarker: null,
     nearby: null,
     list_elem: null,
     amile: function () { return 1609.344; },
     userMarkerImage: null,
     stationMarkerImage: null,
+    currentInfoWindow: null,
     DEBUG: false
 };
 
@@ -16,7 +18,8 @@ jQuery(document).ready(function() {
     var gm = google.maps, myOptions = {
         center: new gm.LatLng(startPos[1], startPos[0]),
         zoom: 8,
-        mapTypeId: gm.MapTypeId.ROADMAP
+        mapTypeId: gm.MapTypeId.ROADMAP,
+        scaleControl: true
     };
     SLF.geocoder = new gm.Geocoder();
     SLF.map = new gm.Map(document.getElementById("map_canvas"), myOptions);
@@ -24,17 +27,21 @@ jQuery(document).ready(function() {
     SLF.userMarkerImage = new gm.MarkerImage(userMarker_url, gm.Size(19, 32));
     SLF.stationMarkerImage = new gm.MarkerImage(stationMarker_url, gm.Size(19, 32));
     SLF.list_elem = $("ul#locations");
-    
-    SLF.locations = initialLocations ? initialLocations : [];
-    
+
+    SLF.locations = initialLocations || [];
+
     SLF.removeStationMarkers = function () {
-        for ( var key in SLF.markers) { SLF.markers[key].setMap(); }
+        for (var key in SLF.markers) { SLF.markers[key].setMap(); }
         SLF.markers = {};
     };
     
     SLF.attachInfoWindow = function (marker, content, open) {
         var infowindow = new gm.InfoWindow({ content: content });
-        gm.event.addListener(marker, 'click', function() { infowindow.open(marker.get('map'), marker); });
+        gm.event.addListener(marker, 'click', function() { 
+            if (SLF.currentInfoWindow !== null) SLF.currentInfoWindow.close();
+            infowindow.open(marker.get('map'), marker); 
+            SLF.currentInfoWindow = infowindow;
+        });
         if (open) infowindow.open(marker.get('map'), marker);
     };
     
@@ -101,12 +108,12 @@ jQuery(document).ready(function() {
         SLF.removeStationMarkers();
         SLF.list_elem.empty();
         var mapBounds = new gm.LatLngBounds();
+        if (SLF.userMarker !== null) mapBounds.extend(SLF.userMarker.getPosition());
         for (var i=0; i < SLF.locations.length; i++) {
             var element = SLF.locations[i];
             var pos = (element.addresses[1] && element.addresses[1].pos) ? element.addresses[1].pos : null;
             var descriptionHTML = SLF.generateDescriptionHTML(element);
             if (pos !== null) {
-                if (SLF.DEBUG) window.log(element.callsign + ' pos ' + pos);
                 var marker= SLF.addMarker(element.callsign, pos, descriptionHTML, SLF.stationMarkerImage);
                 mapBounds.extend(marker.getPosition());
             }
@@ -118,6 +125,7 @@ jQuery(document).ready(function() {
         gm.event.addListenerOnce(SLF.map, 'bounds_changed', function() {
             SLF.revealMarkers(SLF.revealList);    
         });
+        if( SLF.locations.length === 0) SLF.list_elem.append($('<li>').text('No results found with those search parameters!'));
         if (  SLF.locations.length == 1) {  SLF.map.setZoom(12); } else { SLF.map.fitBounds(mapBounds); }
         SLF.updateStationSelect();
     };
@@ -125,44 +133,82 @@ jQuery(document).ready(function() {
     SLF.handleNearbySuccess = function(data, textStatus) {
         if (SLF.DEBUG) window.log('handleNearbySuccess: ' + textStatus);
         SLF.locations = data;
-        if (SLF.locations instanceof Array && SLF.locations.length !== 0) SLF.updateMapApp();
+        if (SLF.locations instanceof Array) SLF.updateMapApp();
     };
     
     SLF.handleNearbyComplete = function(jqXHR, textStatus) {
         if (SLF.DEBUG) window.log('handleNearbyComplete: ' + textStatus);
     };
     
+    SLF.setselectedGeocoderResult = function(location) {
+        if (SLF.userMarker !== null) SLF.userMarker.setMap(null);
+        SLF.selectedGeocoderResult = location;
+        SLF.map.setCenter(SLF.selectedGeocoderResult.geometry.location);
+        var marker = new gm.Marker({
+            map: SLF.map,
+            position: SLF.selectedGeocoderResult.geometry.location,
+            icon: SLF.userMarkerImage,
+            animation: gm.Animation.DROP
+        });
+        SLF.attachInfoWindow(marker, SLF.selectedGeocoderResult.formatted_address);
+        marker.setTitle(SLF.selectedGeocoderResult.formatted_address);
+        SLF.userMarker = marker;
+    };
+    
+    SLF.findNearbyLocations = function() {
+        var radius = $("form#map_form select#radius").val();                    
+        if (SLF.DEBUG) window.log('SLF.findNearbyLocations with radius: ' + radius);
+        $.ajax({
+            url: '/broadcasters/nearby.json',
+            dataType: 'json',
+            data: {'lon': SLF.selectedGeocoderResult.geometry.location.lng(), 'lat': SLF.selectedGeocoderResult.geometry.location.lat(), 'radius': radius},
+            success: SLF.handleNearbySuccess,
+            complete: SLF.handleNearbyComplete
+        });
+    };
+    
     
     $("form#map_form").submit(function(event) {
-        var address = $("form#map_form input#address").val();
-        var city = $("form#map_form input#city").val();
-        var state = $("form#map_form input#state").val();
-        var full_address = address + ' ' + city + ', ' + state;
+        var address = $("form#map_form input#address");
+        var city = $("form#map_form input#city");
+        var state = $("form#map_form input#state");
+        var full_address = address.val() + ' ' + city.val() + ', ' + state.val();
         SLF.geocoder.geocode( {'address': full_address}, function(results, status) 
         {
             if (status == gm.GeocoderStatus.OK) 
             {
                 if (SLF.DEBUG) window.log('GeocoderStatus.OK results:');
                 if (SLF.DEBUG) window.log(results);
-                SLF.searchLoc = results[0].geometry.location;
-                SLF.map.setCenter(SLF.searchLoc);
-                var marker = new gm.Marker({
-                    map: SLF.map,
-                    position: SLF.searchLoc,
-                    icon: SLF.userMarkerImage,
-                    animation: gm.Animation.DROP
-                });
-                SLF.attachInfoWindow(marker, address);
-                marker.setTitle(address);
-                var radius = $("form#map_form select#radius").val();
-                
-                $.ajax({
-                    url: '/broadcasters/nearby.json',
-                    dataType: 'json',
-                    data: {'lon': SLF.searchLoc.lng(), 'lat': SLF.searchLoc.lat(), 'radius': radius},
-                    success: SLF.handleNearbySuccess,
-                    complete: SLF.handleNearbyComplete
-                });
+                if (results.length == 1) {
+                    SLF.setselectedGeocoderResult(results[0]);
+                    SLF.findNearbyLocations();
+                }
+                else
+                {
+                    var location_select = $('<ul class="nav nav-list">');
+                    for (var i=0; i < results.length; i++) {
+                        var anchor = $('<a href="#"></a>').text(results[i].formatted_address);
+                        if (SLF.DEBUG) window.log(anchor);
+                        $(anchor).data('location', results[i]);
+                        location_select.append( $('<li> </li>').append(anchor) );
+                    }
+                    var modal = $('<div class="modal">');
+                    modal.append($('<div class="modal-header">').html('<h3>Multiple results were found, please choose one.</h3>'));
+                    var modal_body = $('<div class="modal-body">').html(location_select);
+                    modal.append(modal_body);
+                    $('body').append(modal);
+                    $(modal).modal({'backdrop': false});
+                    location_select.on('click', 'li a', function(event) {
+                        if (SLF.DEBUG) window.log($(this).data('location'));
+                        $(modal).modal('hide').detach();
+                        var addr_parts = $(this).data('location').formatted_address.split(',');
+                        address.val($.trim(addr_parts[0]));
+                        city.val($.trim(addr_parts[1]));
+                        SLF.setselectedGeocoderResult($(this).data('location'));
+                        SLF.findNearbyLocations();
+                        return false;
+                    });
+                }
 
              } else {
                 if (SLF.DEBUG) window.log("Geocode was not successful for the following reason: " + status);
