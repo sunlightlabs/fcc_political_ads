0df2e87cf93f0bdb619b2e68c5bbaf13bee07bd5
Daniel Cloud <dcloud@sunlightfoundation.com>
2012-05-18 16:01:44 -0400

    Add bootstrapper app in order to implement js plugins from Twitter's bootstrap. Initial widget is a Typeaheadwidget.

---
+++ fcc_adtracker/bootstrapper/widgets.py
@@ -0,0 +1,24 @@
+from django import forms
+
+try:
+    import simplejson as json
+except Exception, e:
+    import json
+
+
+class TypeaheadTextInput(forms.TextInput):
+    attrs = {
+        'class':'typeahead', 
+        'autocomplete': 'off', 
+        'data-provide': 'typeahead',
+        'maxlength': 100
+    }
+
+    def __init__(self, data_source, attrs=None):
+        if not attrs:
+            attrs = self.attrs.copy()
+        if not isinstance(data_source, list):
+            raise TypeError('data_source must be a list')
+        attrs['data-source'] = json.dumps(data_source)
+        super(TypeaheadTextInput, self).__init__(attrs)
+
