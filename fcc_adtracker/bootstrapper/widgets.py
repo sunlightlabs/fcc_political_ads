bdd73abd3b84295a3b8f426839f00d91eaf16b66
Daniel Cloud <dcloud@sunlightfoundation.com>
2012-05-30 15:54:06 -0400

    Updated bootstrapper typeahead widget to take the data-source as an optional kwarg

--- fcc_adtracker/bootstrapper/widgets.py
+++ fcc_adtracker/bootstrapper/widgets.py
@@ -8,17 +8,18 @@ except Exception, e:
 
 class TypeaheadTextInput(forms.TextInput):
     attrs = {
-        'class':'typeahead', 
-        'autocomplete': 'off', 
+        'class':'typeahead',
+        'autocomplete': 'off',
         'data-provide': 'typeahead',
         'maxlength': 100
     }
 
-    def __init__(self, data_source, attrs=None):
+    def __init__(self, data_source=None, attrs=None):
         if not attrs:
             attrs = self.attrs.copy()
-        if not isinstance(data_source, list):
-            raise TypeError('data_source must be a list')
-        attrs['data-source'] = json.dumps(data_source)
+        if data_source:
+            if not isinstance(data_source, list):
+                raise TypeError('data_source must be a list')
+            attrs['data-source'] = json.dumps(data_source)
         super(TypeaheadTextInput, self).__init__(attrs)
 
