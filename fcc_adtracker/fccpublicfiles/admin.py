a66fc742d30bc6afd6add5cf80742ab88afdf5f0
Daniel Cloud <dcloud@sunlightfoundation.com>
2012-06-07 12:02:18 -0400

    Backwards-incompatible rewrite of fccpublicfiles models. Added a few missing fields to PoliticalSpot and PoliticalBuy, and corrected some for null values, etc. Optional Addresses might come back to haunt me. Got rid of South migration history even. Didn't want to write data migrations.

--- fcc_adtracker/fccpublicfiles/admin.py
+++ fcc_adtracker/fccpublicfiles/admin.py
@@ -3,26 +3,30 @@ from django import forms
 from .models import *
 from .views import *
 from django.contrib.admin import widgets
-from bootstrapper.widgets import TypeaheadTextInput
+# from bootstrapper.widgets import TypeaheadTextInput
 
+import weekday_field
 
 CALLSIGNS_LIST = map(lambda x: x[1], CALLSIGNS)
 
+POLITICAL_SPOT_FIELDS = (('airing_start_date', 'airing_end_date', 'airing_days',), ('timeslot_begin', 'timeslot_end'), 'show_name', ('broadcast_length', 'num_spots', 'rate'))
 
 class PoliticalSpotAdminForm(forms.ModelForm):
     class Meta:
         model = PoliticalSpot
-
+    airing_days = weekday_field.forms.WeekdayFormField()
 
 
 class PoliticalSpotAdmin(admin.ModelAdmin):
     form = PoliticalSpotAdminForm
+    
     fieldsets = (
+    
         (None, {
             'fields': ('document',)
         }),
         (None, {
-            'fields': (('airing_start_date', 'airing_end_date'), ('timeslot_begin', 'timeslot_end'), 'show_name', ('broadcast_length', 'num_spots', 'rate'))
+            'fields': POLITICAL_SPOT_FIELDS
         }),
     )
 
@@ -31,9 +35,10 @@ admin.site.register(PoliticalSpot, PoliticalSpotAdmin)
 
 class PoliticalSpotInline(admin.StackedInline):
     model = PoliticalSpot
+    form = PoliticalSpotAdminForm
     fieldsets = (
         (None, {
-            'fields': (('airing_start_date', 'airing_end_date'), ('timeslot_begin', 'timeslot_end'), 'show_name', ('broadcast_length', 'num_spots', 'rate'))
+            'fields': POLITICAL_SPOT_FIELDS
         }),
     )
 
@@ -41,17 +46,35 @@ class PoliticalBuyAdminForm(forms.ModelForm):
     class Meta:
         model = PoliticalBuy
 
-    station = forms.CharField(widget=TypeaheadTextInput())
-    advertiser = forms.CharField(required=False, widget=TypeaheadTextInput())
-    advertiser_signatory = forms.CharField(required=False, widget=TypeaheadTextInput())
+    # station = forms.CharField(widget=TypeaheadTextInput())
+    # advertiser = forms.CharField(required=False, widget=TypeaheadTextInput())
+    # advertiser_signatory = forms.CharField(required=False, widget=TypeaheadTextInput())
 
 class PoliticalBuyAdmin(admin.ModelAdmin):
     form = PoliticalBuyAdminForm
     save_on_top = True
-    list_display = ('documentcloud_doc', 'station', 'advertiser', 'advertiser_signatory', 'ordered_by')
+    list_display = ('documentcloud_doc', 'station', 'advertiser', 'advertiser_signatory', 'bought_by')
     inlines = [
        PoliticalSpotInline,
     ]
 
-
 admin.site.register(PoliticalBuy, PoliticalBuyAdmin)
+
+class RoleAdminInline(admin.StackedInline):
+    model = Role
+    extra = 1
+
+
+class OrganizationAdmin(admin.ModelAdmin):
+    inlines = [
+       RoleAdminInline,
+    ]
+
+class PersonAdmin(admin.ModelAdmin):
+    list_display = ('last_name', 'first_name', 'middle_name')
+    inlines = [
+       RoleAdminInline,
+    ]
+
+admin.site.register(Organization, OrganizationAdmin)
+admin.site.register(Person, PersonAdmin)
--- fcc_adtracker/fccpublicfiles/models.py
+++ fcc_adtracker/fccpublicfiles/models.py
@@ -1,45 +1,107 @@
 from django.db import models
+from django.contrib.localflavor.us.models import USStateField
+
 from doccloud.models import Document
 from broadcasters.models import get_callsigns
 import datetime
 import timedelta
+from weekday_field import fields as wf_fields
 
 CALLSIGNS = [(c,c) for c in get_callsigns()]
+
+ORGANIZATION_TYPES = (
+    (u'MB' ,u'MediaBuyer'),
+    (u'AD', u'Advertiser'),
+)
+
 
 class PublicDocument(models.Model):
     station = models.CharField(choices=CALLSIGNS, max_length=12, verbose_name="Station Callsign")
     documentcloud_doc = models.ForeignKey(Document)
-
+    
     def __unicode__(self):
         if self.documentcloud_doc:
             return u"{0}: {1}".format(self.station, self.documentcloud_doc)
         return u"PublicDocument"
 
 
+class Address(models.Model):
+    address1 = models.CharField(blank=True, null=True, max_length=100)
+    address2 = models.CharField(blank=True, null=True, max_length=100)
+    city = models.CharField(max_length=50)
+    state = USStateField()
+    zipcode = models.CharField(blank=True, null=True, max_length=10)
+
+    def __unicode__(self):
+        if not self.address1 or self.address1 == '':
+            return u"Address"
+        return u"Address: " + self.address1
+
+
+class Person(models.Model):
+    first_name = models.CharField(max_length=40)
+    middle_name = models.CharField(max_length=40, blank=True, null=True)
+    last_name = models.CharField(max_length=40)
+    suffix = models.CharField(max_length=10, blank=True, null=True)
+    
+    class Meta:
+        verbose_name_plural = "People"
+        ordering = ('last_name', 'first_name',)
+    
+    def __unicode__(self):
+        return u"{first_name} {last_name}".format(first_name=self.first_name, last_name=self.last_name)
+
+
+class Role(models.Model):
+    person = models.ForeignKey('Person')
+    organization = models.ForeignKey('Organization')
+    title = models.CharField(max_length=100, help_text="Job title or descriptor for position they hold.")
+    
+    def __unicode__(self):
+        return u"<" + self.person.__unicode__() + ": " + self.title + " >"
+
+
+class Organization(models.Model):
+    name = models.CharField(max_length=100)
+    organization_type = models.CharField(blank=True, max_length=2, choices=ORGANIZATION_TYPES)
+    addresses = models.ManyToManyField(Address, blank=True, null=True)
+    employees = models.ManyToManyField(Person, through=Role)
+    fec_id = models.CharField(max_length=9, blank=True)
+    
+    class Meta:
+        ordering = ('name',)
+    
+    def __unicode__(self):
+        if self.name:
+            return self.name
+        return u"Organization"
+
 
 class PoliticalBuy(PublicDocument):
     """A subset of PublicFile, the PoliticalBuy records purchases of air time (generally for political ads)"""
-    contract_number = models.CharField(blank=True, null=True, max_length=100)
-    advertiser = models.CharField(blank=True, null=True, max_length=100)
-    advertiser_signatory =  models.CharField(blank=True, null=True, max_length=100)
-    ordered_by = models.CharField(blank=True, null=True, max_length=100)
+    contract_number = models.CharField(blank=True, max_length=100)
+    advertiser = models.ForeignKey('Organization', blank=True, null=True, related_name='advertiser_politicalbuys', limit_choices_to={'organization_type' : u'AD'})
+    advertiser_signatory = models.ForeignKey('Person', blank=True, null=True)
+    bought_by = models.ForeignKey('Organization', blank=True, null=True, related_name='mediabuyer_politicalbuys', limit_choices_to={'organization_type' : u'MB'})
     contract_start_date = models.DateField(blank=True, null=True, default=datetime.datetime.today)
     contract_end_date = models.DateField(blank=True, null=True, default=datetime.datetime.today)
-
+    lowest_unit_price = models.NullBooleanField(default=None, blank=True, null=True)
 
 
 class PoliticalSpot(models.Model):
     """Information particular to a political ad spot (e.g., a candidate ad)"""
     document = models.ForeignKey(PoliticalBuy, verbose_name="Political Buy")
-    airing_start_date = models.DateField(blank=True, null=True, default=datetime.datetime.today)
-    airing_end_date = models.DateField(blank=True, null=True, default=datetime.datetime.today)
+    airing_start_date = models.DateField(blank=True, null=True)
+    airing_end_date = models.DateField(blank=True, null=True)
+    airing_days = wf_fields.WeekdayField(blank=True)
     timeslot_begin = models.TimeField(blank=True, null=True)
     timeslot_end = models.TimeField(blank=True, null=True)
-    show_name = models.CharField(blank=True, null=True, max_length=100)
+    show_name = models.CharField(blank=True, max_length=100)
     broadcast_length = timedelta.TimedeltaField(blank=True, null=True, help_text="The easiest way to enter time is as <em>XX minutes, YY seconds</em> or <em>YY seconds</em>.")
     num_spots = models.IntegerField(blank=True, null=True, verbose_name="Number of Spots")
     rate = models.DecimalField(blank=True, null=True, max_digits=9, decimal_places=2, help_text="Dollar cost for each spot")
-
+    preemptable = models.NullBooleanField(default=None, blank=True, null=True)
+    
     def documentcloud_doc():
         doc = "The documentcloud_doc property."
         def fget(self):
