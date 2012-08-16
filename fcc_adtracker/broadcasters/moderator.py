38711fbd67b115a95fed42a6c6950967f1fb81a9
Daniel Cloud <dcloud@sunlightfoundation.com>
2012-08-16 12:45:28 -0400

    Added migration for broadcasters and added moderator

---
+++ fcc_adtracker/broadcasters/moderator.py
@@ -0,0 +1,10 @@
+from moderation import moderation
+from moderation.moderator import GenericModerator
+from broadcasters.models import Broadcaster, BroadcasterAddress
+
+
+class DefaultModerator(GenericModerator):
+    auto_approve_for_staff = False
+
+moderation.register(Broadcaster, DefaultModerator)
+moderation.register(BroadcasterAddress, DefaultModerator)
