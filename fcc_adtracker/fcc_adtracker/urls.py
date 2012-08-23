75ceb3d3972e499c39b5e930371af28930cce4c1
Daniel Cloud <dcloud@sunlightfoundation.com>
2012-08-23 10:32:36 -0400

    Picked home.html template, view from master as transition to new signup forms

--- fcc_adtracker/fcc_adtracker/urls.py
+++ fcc_adtracker/fcc_adtracker/urls.py
@@ -1,6 +1,4 @@
 from django.conf.urls import patterns, include, url
-from django.views.generic import TemplateView
-from fcc_adtracker.views import HomePageView
 from broadcasters.urls import json_urlpatterns
 from ajax_select import urls as ajax_select_urls
 from fccpublicfiles.views import prelim_doc_form
@@ -17,7 +15,7 @@ urlpatterns = patterns('',
     # url(r'^publicfiles/', include('fccpublicfiles.urls')),
     url(r'^stations/', include(json_urlpatterns)),
     url(r'^states/(?P<state_id>\w{2})/$', 'broadcasters.views.state_broadcaster_list', name='state_broadcaster_list'),
-    url(r'^$', 'broadcasters.views.featured_broadcasters', name='home'),
+    url(r'^$', 'fcc_adtracker.views.home_view', name='home'),
     (r'^admin/lookups/', include(ajax_select_urls)),
     url(r'^admin/fccpublicfiles/', include('fccpublicfiles.admin_urls')),
     url(r'^admin/', include(admin.site.urls)),
