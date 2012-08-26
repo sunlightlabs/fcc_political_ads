97823740df460d4caeef1dd94e3d85326aa4d0a3
Daniel Cloud <dcloud@sunlightfoundation.com>
2012-08-25 23:47:53 -0400

    Updated home to use nonuserprofileform.

--- fcc_adtracker/fcc_adtracker/views.py
+++ fcc_adtracker/fcc_adtracker/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from django.contrib.localflavor.us import us_states
+from volunteers.forms import NonUserProfileForm
 
 
 def home_view(request):
     resp_obj = {
+        'form': NonUserProfileForm,
         'states_dict': us_states.US_STATES,
         'sfapp_base_template': 'sfapp/base-full.html'
     }
