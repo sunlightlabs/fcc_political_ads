505068f0f72915010335976ec4da451ff56385d0
Daniel Cloud <dcloud@sunlightfoundation.com>
2012-08-30 18:54:49 -0400

    Added js pager to tablesorter js.

--- fcc_adtracker/fcc_adtracker/project_static/js/sort_table.js
+++ fcc_adtracker/fcc_adtracker/project_static/js/sort_table.js
@@ -1,6 +1,19 @@
 jQuery(document).ready(function($) {
-    $('table.sortable').tablesorter({
-         debug: true,
-         cssChildRow: "row-child"
-    });
+    var table_el = $('table.sortable');
+    var pagerOptions = {
+        container: $(".pager"),
+        page: 0,
+        size: 20
+    };
+    var rowCount = table_el.children('tbody > tr').length;
+    // if (rowCount > pagerOptions.size) {
+        table_el.tablesorter({
+             debug: true,
+             cssChildRow: "row-child"
+        }).tablesorterPager(pagerOptions);
+    // }
+    // else
+    // {
+    //     $('.pager').hide();
+    // }
 });
