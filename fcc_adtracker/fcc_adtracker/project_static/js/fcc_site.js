bb7a108aff916c6e77ec49d614f0fe5b2d25d773
Daniel Cloud <dcloud@sunlightfoundation.com>
2012-09-05 17:38:42 -0400

    Updated version of boostrap.js for use in typeaheads (there was a bug in newer FF that this would fix). Typeaheads still have problem of "when does typeahead know we need to create a new related object?". See commit 9dbe810 and issue #37. JS that implements typeahead is in fcc_site.js, but commented out as it does not address issue described above.

--- fcc_adtracker/fcc_adtracker/project_static/js/fcc_site.js
+++ fcc_adtracker/fcc_adtracker/project_static/js/fcc_site.js
@@ -3,7 +3,7 @@ window.log = function f(){ log.history = log.history || []; log.history.push(arg
 (function(a){function b(){}for(var c="assert,count,debug,dir,dirxml,error,exception,group,groupCollapsed,groupEnd,info,log,markTimeline,profile,profileEnd,time,timeEnd,trace,warn".split(","),d;!!(d=c.pop());){a[d]=a[d]||b;}})
 (function(){try{console.log();return window.console;}catch(a){return (window.console={});}}());
 
-$(document).ready(function() {
+jQuery(document).ready(function($) {
     if (!Modernizr.input.placeholder) {
         $(this).find('[placeholder]').each(function()
         {
@@ -21,13 +21,13 @@ $(document).ready(function() {
                 }
             }).blur(function()
             {
-                if ($(this).val() == '' || $(this).val() == $(this).attr('placeholder'))
+                if ($(this).val() === '' || $(this).val() == $(this).attr('placeholder'))
                 {
                     $(this).val($(this).attr('placeholder'));
                     $(this).addClass('placeholder');
                 }
             });
- 
+
             // remove placeholders on submit
             $('[placeholder]').closest('form').submit(function()
             {
@@ -37,7 +37,42 @@ $(document).ready(function() {
                     {
                         $(this).val('');
                     }
-                })
+                });
             });
     }
+
+    /*
+        Typeahead transformations for select forms
+        depends on underscore.js
+    */
+    /*
+    var transform_list = jQuery.unique($('select.suggestions'));
+    transform_list.each(function(index) {
+        var select_el = $(this);
+        var option_dict = {}, typeahead_labels = [], initial_label = select_el.children(':selected').eq(0).val() !== '' ? select_el.children(':selected').eq(0).text() : '';
+        var proxy_id = 'proxy-'+ select_el.attr('id');
+        var input_proxy = $('<input id="'+ proxy_id + '" type="text" data-provide="typeahead">').val(initial_label).addClass('typeahead select_proxy');
+        if (!jQuery.contains(document, input_proxy[0])) {
+            $(this).children('option').each(function(index) {
+                if ($(this).val() !== '') {
+                    typeahead_labels.push($(this).text());
+                    option_dict[$(this).text()] = $(this).val();
+                }
+            });
+            input_proxy.data('option_dict', option_dict);
+            $(input_proxy).typeahead({
+                source: typeahead_labels,
+                items: 14
+            })
+            .change(function(event) {
+                var new_label = $(this).val();
+                select_el.val(option_dict[new_label]);
+            });
+            $(this).hide();
+            $(this).before(input_proxy);
+        }
+
+    });
+*/
+
 });
