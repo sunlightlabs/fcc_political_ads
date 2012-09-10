380a5a24a6ec68f26fe8586ff1717d489f99d23d
Alison Rowland <arowland@sunlightfoundation.com>
2012-09-10 16:52:16 -0400

    Fixes #63: Make password fields into real password form widgets

--- fcc_adtracker/stronger_auth/forms.py
+++ fcc_adtracker/stronger_auth/forms.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.forms import SetPasswordForm, PasswordChangeForm
 from django.utils.translation import ugettext_lazy as _
 from django.conf import settings
+from django.forms.widgets import PasswordInput
 
 from passwords.fields import PasswordField
 
@@ -8,10 +9,10 @@ PASSWORD_HELP_TEXT = getattr(settings, 'PASSWORD_HELP_TEXT', u'Please choose a s
 
 
 class HelpfulPasswordField(PasswordField):
-    def __init__(self, required=True, widget=None, label=None, initial=None,
+    def __init__(self, required=True, widget=PasswordInput, label=None, initial=None,
                  help_text=None, error_messages=None, show_hidden_initial=False,
                  validators=[], localize=False):
-        super(HelpfulPasswordField, self).__init__(required=True, widget=None, label=None, initial=None,
+        super(HelpfulPasswordField, self).__init__(required=True, widget=widget, label=None, initial=None,
                  help_text=None, error_messages=None, show_hidden_initial=False,
                  validators=[], localize=False)
         self.help_text = PASSWORD_HELP_TEXT
