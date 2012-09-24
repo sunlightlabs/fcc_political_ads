b37898f97638504afebdc91c6ce4304d1933a063
Jacob Fenton <jfenton@sunlightfoundation.com>
2012-09-24 15:50:43 -0400

    add local_log

---
+++ fcc_adtracker/scraper/local_log.py
@@ -0,0 +1,31 @@
+
+import logging
+from django.conf import settings
+
+LOG_NAME = "scrape_log"
+FCC_SCRAPER_LOG = getattr(settings, 'FCC_SCRAPER_LOG')
+""" Set up a logger with settings from the settings file. Will log info and anything higher. 
+
+
+my_logger=fec_logger()
+my_logger.info('my_logger.info')
+my_logger.warn('my_logger.warn')
+my_logger.error('my_logger.error')
+my_logger.critical('my_logger.critical')
+
+-
+
+2012-04-17 13:24:46,745 INFO my_logger.info
+2012-04-17 13:24:46,745 WARNING my_logger.warn
+2012-04-17 13:24:46,745 ERROR my_logger.error
+2012-04-17 13:24:46,745 CRITICAL my_logger.critical
+
+"""
+def fcc_logger():
+    logger = logging.getLogger(LOG_NAME)
+    hdlr = logging.FileHandler(FCC_SCRAPER_LOG)
+    formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
+    hdlr.setFormatter(formatter)
+    logger.addHandler(hdlr) 
+    logger.setLevel(logging.INFO)
+    return logger
