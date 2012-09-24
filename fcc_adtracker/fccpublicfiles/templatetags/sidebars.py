d538df43dcfca3e9a71aaf17f24507c45633de85
Jacob Fenton <jfenton@sunlightfoundation.com>
2012-09-24 15:30:29 -0400

    change order

--- fcc_adtracker/fccpublicfiles/templatetags/sidebars.py
+++ fcc_adtracker/fccpublicfiles/templatetags/sidebars.py
@@ -7,7 +7,7 @@ register = template.Library()
 
 @register.inclusion_tag('recent_documents_sidebar.html')
 def recent_buy_sidebar():
-    ad_buys = PoliticalBuy.objects.public().order_by('created_at')[:MAX_DOCUMENTS_TO_SHOW_IN_SIDEBAR]
+    ad_buys = PoliticalBuy.objects.public().order_by('-created_at')[:MAX_DOCUMENTS_TO_SHOW_IN_SIDEBAR]
     return {
     'ad_buys':ad_buys,
     }
