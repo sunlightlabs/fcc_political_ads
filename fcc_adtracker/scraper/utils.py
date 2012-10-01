ba1d8ba619dae64fc17e001ea5cd773cccdd346a
Daniel Cloud <dcloud@sunlightfoundation.com>
2012-10-01 11:56:56 -0400

    Add some defaults for getattr in utils, views for scraper app. PEP8 cleanup. Put needed settings in local_settings.py.default

--- fcc_adtracker/scraper/utils.py
+++ fcc_adtracker/scraper/utils.py
@@ -10,11 +10,11 @@ from django.contrib.auth.models import User
 from django.core.files import File
 
 
-SCRAPER_USER_AGENT_STRING = getattr(settings, 'SCRAPER_USER_AGENT_STRING')
+SCRAPER_USER_AGENT_STRING = getattr(settings, 'SCRAPER_USER_AGENT_STRING', 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/535.7 (KHTML, like Gecko) Chrome/16.0.912.36 Safari/535.7')
 
 
 def read_url(url):
-    # read url with our headers. 
+    # read url with our headers.
     opener = urllib2.build_opener()
     opener.addheaders = [('User-agent', SCRAPER_USER_AGENT_STRING)]
     parsed_url = urlparse(url)
@@ -22,7 +22,7 @@ def read_url(url):
     print("trying to read url: %s" % (final_url))
     page = opener.open(final_url).read()
     return page
-    
+
 # assumes we're looking at 2012
 folder_url_re = re.compile(r'https://stations.fcc.gov/station-profile/(.*?)/political-files/browse->(.*)')
 file_url_re = re.compile(r'https://stations.fcc.gov/collect/files/(\d+)/Political File/(.+)')
@@ -31,7 +31,7 @@ def clean_path(url):
     url = url.replace('&gt;', '>')
     url = url.replace('%3e', '>')
     return url
-    
+
 def parse_folder_url(url):
     url = clean_path(url)
     url_parts = re.findall(folder_url_re, url)
@@ -40,16 +40,16 @@ def parse_folder_url(url):
         callSign = url_parts[0][0]
         path = url_parts[0][1]
         pathArray = path.split('->')
-    else: 
+    else:
         print "Couldn't parse folder file path %s" % (url)
-        
+
     return callSign, pathArray
 
 def get_folder_path(url):
     url = clean_path(url)
     (callSign, PathArray) = parse_folder_url(url)
     return PathArray
-    
+
 def parse_file_url(url):
     url_parts = re.findall(file_url_re, url)
     (fac_id, pathArray) = (None, None)
@@ -57,15 +57,15 @@ def parse_file_url(url):
         fac_id = url_parts[0][0]
         path = url_parts[0][1]
         pathArray = path.split('/')
-    else: 
+    else:
         print "Couldn't parse file path %s" % (url)
-        
+
     return fac_id, pathArray
 
 def get_file_path(url):
     (fac_id, PathArray) = parse_file_url(url)
     return PathArray
-    
-# List of stations that are mandated--we think--to post their political files. ABC, NBC, CBS and Fox affiliates in the top 50 DMA's should be on this list. Unsure about: can stations be 'partial' affiliates, or have agreements with major networks that don't rise to level of formal affiliation? As of this writing stations are either 'all in' or 'all out'; other stations can voluntarily upload their files, but if so they are supposed to upload all of them. Not sure if this has been formalized. 
+
+# List of stations that are mandated--we think--to post their political files. ABC, NBC, CBS and Fox affiliates in the top 50 DMA's should be on this list. Unsure about: can stations be 'partial' affiliates, or have agreements with major networks that don't rise to level of formal affiliation? As of this writing stations are either 'all in' or 'all out'; other stations can voluntarily upload their files, but if so they are supposed to upload all of them. Not sure if this has been formalized.
 mandated_stations = ['WABC-TV', 'WCBS-TV', 'WNBC', 'WNYW', 'KABC-TV', 'KCBS-TV', 'KNBC', 'KTTV', 'KWHY-TV', 'WBBM-TV', 'WFLD', 'WLS-TV', 'WMAQ-TV', 'WMGM-TV', 'KYW-TV', 'WCAU', 'WPVI-TV', 'WTXF-TV', 'KDFW', 'KTVT', 'KXAS-TV', 'WFAA', 'KGO-TV', 'KNTV', 'KPIX-TV', 'KTVU', 'WBZ-TV', 'WCVB-TV', 'WFXT', 'WHDH', 'WMUR-TV', 'WJLA-TV', 'WRC-TV', 'WTTG', 'WUSA', 'WHAG-TV', 'WAGA-TV', 'WGCL-TV', 'WSB-TV', 'WXIA-TV', 'KHOU', 'KPRC-TV', 'KRIV', 'KTRK-TV', 'WDIV-TV', 'WJBK', 'WWJ-TV', 'WXYZ-TV', 'KCPQ', 'KING-TV', 'KIRO-TV', 'KOMO-TV', 'KNAZ-TV', 'KNXV-TV', 'KPHO-TV', 'KPNX', 'KSAZ-TV', 'WFLA-TV', 'WFTS-TV', 'WTSP', 'WTVT', 'WWSB', 'KARE', 'KCCO-TV', 'KCCW-TV', 'KFTC', 'KMSP-TV', 'KRWF', 'KSAX', 'KSTP-TV', 'WCCO-TV', 'WFOR-TV', 'WPLG', 'WSVN', 'WTVJ', 'KCNC-TV', 'KDVR',  'KFCT', 'KMGH-TV', 'KREG-TV', 'KUSA', 'KQCK', 'WEWS-TV', 'WJW', 'WKYC', 'WOIO', 'WESH', 'WFTV', 'WKMG-TV', 'WOFL', 'WRDQ', 'KCRA-TV', 'KOVR', 'KTXL', 'KXTV', 'KDNL-TV', 'KMOV', 'KSDK', 'KTVI', 'WCSH', 'WGME-TV', 'WMTW', 'WPFO', 'KDKA-TV', 'WPGH-TV', 'WPXI', 'WTAE-TV', 'WNCN', 'WRAL-TV', 'WRAZ', 'WTVD', 'WBTV', 'WCCB', 'WCNC-TV', 'WSOC-TV', 'WISH-TV', 'WRTV', 'WTHR', 'WXIN', 'WBAL-TV', 'WBFF', 'WJZ-TV', 'WMAR-TV', 'KFMB-TV', 'KGTV', 'KNSD', 'KSWB-TV', 'WKRN-TV', 'WSMV-TV', 'WTVF', 'WZTV', 'WFSB', 'WTIC-TV', 'WTNH', 'WVIT', 'KCTV', 'KMBC-TV', 'KSHB-TV', 'WDAF-TV', 'WBNS-TV', 'WCMH-TV', 'WSYX', 'WTTE', 'KENV-DT', 'KVNV', 'KMYU', 'KSL-TV', 'KSTU', 'KTVX', 'KUTV', 'KGWR-TV', 'WDJT-TV', 'WISN-TV', 'WITI', 'WTMJ-TV', 'WXIX-TV', 'WCPO-TV', 'WKRC-TV', 'WLWT', 'KABB', 'KENS', 'KSAT-TV', 'WOAI-TV', 'WUGA-TV', 'WLOS', 'WHNS', 'WSPA-TV', 'WFLX', 'WPBF', 'WPEC', 'WPTV-TV', 'WBRC', 'WCFT-TV', 'WIAT', 'WJSU-TV', 'WVTM-TV', 'KLAS-TV', 'KSNV-DT', 'KTNV-TV', 'KVVU-TV', 'WGAL', 'WHP-TV', 'WHTM-TV', 'WPMT', 'WOOD-TV', 'WOTV', 'WWMT', 'WXMI', 'WZZM', 'WAVY-TV', 'WTKR', 'WVBT', 'WVEC', 'KFOR-TV', 'KOCO-TV', 'KOKH-TV', 'KWTV-DT', 'KREZ-TV', 'KASA-TV', 'KBIM-TV', 'KOAT-TV', 'KOB', 'KOBF', 'KOBR', 'KRQE', 'WFMY-TV', 'WGHP', 'WXII-TV', 'WXLV-TV', 'KBVO', 'KEYE-TV', 'KTBC', 'KVUE', 'KXAN-TV', 'WAVE', 'WDRB', 'WHAS-TV', 'WLKY-TV', 'WHBQ-TV', 'WMC-TV', 'WPTY-TV', 'WREG-TV', 'WAWS', 'WJXX', 'WTEV-TV', 'WTLV']
 
--- fcc_adtracker/scraper/views.py
+++ fcc_adtracker/scraper/views.py
@@ -1,6 +1,7 @@
 import datetime
 import csv
 
+from django.http import Http404
 from django.shortcuts import render_to_response, redirect
 
 from models import StationData, PDF_File, dma_summary, state_summary
@@ -11,25 +12,26 @@ from django.contrib.localflavor.us import us_states
 
 from django.conf import settings
 from django.views.decorators.cache import cache_page
-CSV_EXPORT_DIR = getattr(settings, 'CSV_EXPORT_DIR')
-
+CSV_EXPORT_DIR = getattr(settings, 'CSV_EXPORT_DIR', 'csv_exports')
 
 
 STATES_DICT = dict(us_states.US_STATES)
-CACHE_TIME = 15 * 60 
+CACHE_TIME = 15 * 60
+
 
 @cache_page(CACHE_TIME)
 def state_fcc_list(request):
-    
+
     states = state_summary.objects.filter(tot_buys__gte=0).order_by('-tot_buys')
 
     return render_to_response('geography_list.html', {
-        'geography_name':'state',
-        'geography_name_short':'state',
-        'geography_list':states,
+        'geography_name': 'state',
+        'geography_name_short': 'state',
+        'geography_list': states,
         'sfapp_base_template': 'sfapp/base-full.html',
-        
+
     })
+
 
 @cache_page(CACHE_TIME)
 def recent_state_fcc_list(request):
@@ -37,75 +39,80 @@ def recent_state_fcc_list(request):
     states = state_summary.objects.filter(tot_buys__gte=0).order_by('-tot_buys')
 
     return render_to_response('recent_geography_list.html', {
-        'geography_name':'state',
-        'geography_name_short':'state',
-        'geography_list':states,
+        'geography_name': ' state',
+        'geography_name_short': 'state',
+        'geography_list': states,
         'sfapp_base_template': 'sfapp/base-full.html',
 
     })
-    
+
+
 @cache_page(CACHE_TIME)
 def dma_fcc_list(request):
 
     dmas = dma_summary.objects.filter(tot_buys__gte=0).order_by('-tot_buys')
     return render_to_response('geography_list.html', {
-        'geography_name':'TV market',
-        'geography_name_short':'dma',
-        'geography_list':dmas,
+        'geography_name': 'TV market',
+        'geography_name_short': 'dma',
+        'geography_list': dmas,
         'sfapp_base_template': 'sfapp/base-full.html',
     })
+
 
 @cache_page(CACHE_TIME)
 def recent_dma_fcc_list(request):
 
     dmas = dma_summary.objects.filter(tot_buys__gte=0).order_by('-tot_buys')
     return render_to_response('recent_geography_list.html', {
-        'geography_name':'TV market',
-        'geography_name_short':'dma',
-        'geography_list':dmas,
+        'geography_name': 'TV market',
+        'geography_name_short': 'dma',
+        'geography_list': dmas,
         'sfapp_base_template': 'sfapp/base-full.html',
     })
+
 
 @cache_page(CACHE_TIME)
 def station_fcc_list(request):
 
     broadcasters = StationData.objects.filter(is_mandated_station=True).order_by('callSign').values('callSign', 'networkAfil', 'communityCity', 'communityState', 'nielsenDma')
-    
+
     for broadcaster in broadcasters:
         broadcaster['geography_name'] = "%s (%s)" % (broadcaster['callSign'], broadcaster['networkAfil'])
         broadcaster['geography_name_short'] = broadcaster['callSign']
         broadcaster['location1'] = "%s, %s" % (broadcaster['communityCity'], broadcaster['communityState'])
         broadcaster['location2'] = broadcaster['nielsenDma']
     return render_to_response('broadcaster_list.html', {
-        'geography_name':'TV station',
-        'geography_name_short':'tv-station',
-        'geography_list':broadcasters,
-        'show_location':'True',
+        'geography_name': 'TV station',
+        'geography_name_short': 'tv-station',
+        'geography_list': broadcasters,
+        'show_location': 'True',
     })
 
-@cache_page(CACHE_TIME)  
+
+@cache_page(CACHE_TIME)
 def station_state_list(request, state_id):
     state_name = STATES_DICT.get(state_id, None)
     if state_name:
         broadcasters = StationData.objects.filter(is_mandated_station=True, communityState=state_id).order_by('callSign').values('callSign', 'networkAfil', 'communityCity', 'communityState', 'nielsenDma')
-        
+
         for broadcaster in broadcasters:
             broadcaster['geography_name'] = "%s (%s)" % (broadcaster['callSign'], broadcaster['networkAfil'])
             broadcaster['geography_name_short'] = broadcaster['callSign']
             broadcaster['location1'] = "%s" % (broadcaster['communityCity'])
             broadcaster['location2'] = broadcaster['nielsenDma']
         return render_to_response('broadcaster_list.html', {
-            'geography_name':'TV station',
-            'geography_name_short':'tv-station',
-            'geography_list':broadcasters,
-            'show_location':'True',
-            'subgeography':state_name,
+            'geography_name': 'TV station',
+            'geography_name_short': 'tv-station',
+            'geography_list': broadcasters,
+            'show_location': 'True',
+            'subgeography': state_name,
         })
-        
+
     else:
         raise Http404('State with abbrevation "{state_id}" not found.'.format(state_id=state_id))
 
-@cache_page(CACHE_TIME)      
+
+@cache_page(CACHE_TIME)
 def station_dma_list(request, dma_id):
     dma_name = None
 
@@ -114,18 +121,19 @@ def station_dma_list(request, dma_id):
     for broadcaster in broadcasters:
         if not dma_name:
             dma_name = broadcaster['nielsenDma']
-        
+
         broadcaster['geography_name'] = "%s (%s)" % (broadcaster['callSign'], broadcaster['networkAfil'])
         broadcaster['geography_name_short'] = broadcaster['callSign']
         broadcaster['location1'] = "%s, %s" % (broadcaster['communityCity'], broadcaster['communityState'])
         broadcaster['location2'] = broadcaster['nielsenDma']
     return render_to_response('broadcaster_list.html', {
-        'geography_name':'TV station',
-        'geography_name_short':'tv-station',
-        'geography_list':broadcasters,
-        'show_location':'True',
-        'subgeography':dma_name,
+        'geography_name': 'TV station',
+        'geography_name_short': 'tv-station',
+        'geography_list': broadcasters,
+        'show_location': 'True',
+        'subgeography': dma_name,
     })
+
 
 @cache_page(CACHE_TIME)
 def filing_dma_list(request, dma_id):
@@ -136,14 +144,15 @@ def filing_dma_list(request, dma_id):
     if filings:
         dma_name = filings[0].folder.broadcaster.nielsen_dma
     return render_to_response('filing_list.html', {
-        'filings':filings,
-        'geography_name':dma_name,
-        'preposition':'in',
-        'count':count,
+        'filings': filings,
+        'geography_name': dma_name,
+        'preposition': 'in',
+        'count': count,
         'sfapp_base_template': 'sfapp/base-full.html',
-    }) 
-  
-@cache_page(CACHE_TIME)  
+    })
+
+
+@cache_page(CACHE_TIME)
 def filing_station_list(request, callsign):
 
     filings = PDF_File.objects.filter(callsign__iexact=callsign).order_by('-upload_time')
@@ -155,16 +164,17 @@ def filing_station_list(request, callsign):
         pass
 
     return render_to_response('filing_list.html', {
-        'filings':filings,
-        'geography_name':callsign,
-        'preposition':'from',
-        'count':count,
-        'broadcaster':broadcaster,
+        'filings': filings,
+        'geography_name': callsign,
+        'preposition': 'from',
+        'count': count,
+        'broadcaster': broadcaster,
         'sfapp_base_template': 'sfapp/base-full.html',
-        
-    })       
 
-@cache_page(CACHE_TIME)   
+    })
+
+
+@cache_page(CACHE_TIME)
 def filing_state_list(request, state_id):
     state_name = STATES_DICT.get(state_id, None)
     if state_name:
@@ -173,34 +183,35 @@ def filing_state_list(request, state_id):
         count = filings.aggregate(numfilings=Count('pk'))['numfilings']
 
         return render_to_response('filing_list.html', {
-            'filings':filings,
-            'geography_name':state_name,
-            'preposition':'in',
-            'count':count,
+            'filings': filings,
+            'geography_name': state_name,
+            'preposition': 'in',
+            'count': count,
             'sfapp_base_template': 'sfapp/base-full.html',
-        }) 
-        
+        })
+
     else:
         raise Http404('State with abbrevation "{state_id}" not found.'.format(state_id=state_id))
- 
-@cache_page(CACHE_TIME)       
+
+
+@cache_page(CACHE_TIME)
 def fcc_most_recent(request):
-    
+
     today = datetime.datetime.today()
     one_week_ago = today - datetime.timedelta(days=7)
-    
+
     filings = PDF_File.objects.filter(upload_time__gte=one_week_ago).order_by('-upload_time')
     count = filings.aggregate(numfilings=Count('pk'))['numfilings']
-    
+
     return render_to_response('filing_list.html', {
-        'filings':filings,
-        'geography_name':'the last week',
-        'preposition':'in',
-        'count':count,
+        'filings': filings,
+        'geography_name': 'the last week',
+        'preposition': 'in',
+        'count': count,
         'sfapp_base_template': 'sfapp/base-full.html',
     })
+
 
 def ad_buy_redirect(request, buy_id):
     adbuy = PoliticalBuy.objects.get(related_FCC_file__pk=buy_id)
     return redirect(adbuy.get_absolute_url())
-    
