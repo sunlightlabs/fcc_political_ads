ce165cbee78d16438d9acff274812e83fc1202a0
Daniel Cloud <dcloud@sunlightfoundation.com>
2012-10-01 18:17:44 -0400

    Fix to make date search work. Somehow indexes on start_date and end_date didn't end up in my commits from before? Or something, sorry.

--- fcc_adtracker/search/forms.py
+++ fcc_adtracker/search/forms.py
@@ -17,10 +17,10 @@ class DateRangeSearchForm(FacetedSearchForm):
         if len(sqs) > 0:
             # Check to see if a start_date was chosen.
             if self.cleaned_data['start_date']:
-                sqs = sqs.filter(contract_start_date__gte=self.cleaned_data['start_date'])
+                sqs = sqs.filter(start_date__gte=self.cleaned_data['start_date'])
 
             # Check to see if an end_date was chosen.
             if self.cleaned_data['end_date']:
-                sqs = sqs.filter(contract_end_date__lte=self.cleaned_data['end_date'])
+                sqs = sqs.filter(end_date__lte=self.cleaned_data['end_date'])
 
         return sqs
--- fcc_adtracker/search/search_indexes.py
+++ fcc_adtracker/search/search_indexes.py
@@ -12,13 +12,14 @@ class PoliticalBuyIndex(indexes.SearchIndex, indexes.Indexable):
     type = indexes.CharField(faceted=True, default='')
     relatedfccfile = indexes.CharField(model_attr='related_FCC_file', null=True, default='', faceted=False)
     advertiser = indexes.CharField(model_attr='advertiser', null=True, default='Unknown', faceted=True)
+    start_date = indexes.DateField(model_attr='contract_start_date')
+    end_date = indexes.DateField(model_attr='contract_end_date')
 #    advertiser_signatory = indexes.CharField(model_attr='advertiser_signatory', null=True, default='Unknown', faceted=True)
 #    media_buyer = indexes.CharField(model_attr='bought_by', null=True, default='Unknown', faceted=False)
     state = indexes.MultiValueField(faceted=True)
-#    station = indexes.MultiValueField(faceted=True)
+    # station = indexes.MultiValueField(faceted=True)
     source = indexes.CharField(faceted=True, default='')
     status = indexes.CharField(faceted=True, default='')
-
 
     def get_model(self):
         return PoliticalBuy
@@ -29,22 +30,22 @@ class PoliticalBuyIndex(indexes.SearchIndex, indexes.Indexable):
     def index_queryset(self):
         """Used when the entire index for model is updated."""
         return self.get_model().objects.all()
-        
+
     def prepare_type(self, obj):
         if obj.is_FCC_doc:
             return obj.related_FCC_file.candidate_type()
         else:
             return 'Unknown'
+
     def prepare_source(self, obj):
         return obj.doc_source()
-    
+
 #    def prepare_station(self, obj):
 #        return obj.broadcasters_callsign_list()
-    
+
     def prepare_state(self, obj):
         return obj.broadcasters_state_list()
-        
+
     def prepare_status(self, obj):
         #print "Preparing status: %s - status is '%s'" % (obj, obj.doc_status())
         return obj.doc_status()
-    
