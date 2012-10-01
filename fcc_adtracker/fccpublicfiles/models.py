ad30522cf0a60d0de4c1d03c2a1f163a64ddbf6d
Jacob Fenton <jfenton@sunlightfoundation.com>
2012-10-01 16:51:42 -0400

    fix date display, maybe

--- fcc_adtracker/fccpublicfiles/models.py
+++ fcc_adtracker/fccpublicfiles/models.py
@@ -155,7 +155,11 @@ class PoliticalBuy(MildModeratedModel):
         return self.advertiser or 'Unknown'
 
     def date_display(self):
-        date_str = u"{0}".format(self.contract_end_date.strftime("%m/%d/%y"))
+        date_str = ''
+        if self.is_FCC_doc:
+            date_str = u"{0}".format(self.related_FCC_file.upload_time.strftime("%Y%m%d"))
+        else:
+            date_str = u"{0}".format(self.contract_end_date.strftime("%m/%d/%y"))
         return date_str
 
     def citystate_display(self):
