d34b8924172a9e1422ac78a5ec8bb4a4b23aa92b
Daniel Cloud <dcloud@sunlightfoundation.com>
2012-10-11 10:43:58 -0400

    Fixed bug with volunteer not being able to set profile password when an original password not set (registered via social auth). Added note in template to clarify.

--- fcc_adtracker/volunteers/views.py
+++ fcc_adtracker/volunteers/views.py
@@ -137,11 +137,12 @@ def edit_profile(request):
             if still_valid:
 
                 # update password
+                has_usable_pw = user.has_usable_password()
 
                 new_pw = form.cleaned_data.get('new_password')
                 new_pw_confirm = form.cleaned_data.get('new_password_confirm')
 
-                if new_pw and user.has_usable_password:
+                if new_pw and has_usable_pw:
                     old_pw = form.cleaned_data.get('old_password')
 
                     if not user.check_password(old_pw):
@@ -157,6 +158,16 @@ def edit_profile(request):
                         else:
                             form.errors['new_password_confirm'] = ['Please confirm your new password.']
                             still_valid = False
+                elif new_pw:
+                    if new_pw_confirm:
+                        if new_pw == new_pw_confirm:
+                            user.set_password(form.cleaned_data['new_password'])
+                        else:
+                            form.errors['new_password'] = ['Password and confirmation did not match.']
+                            still_valid = False
+                    else:
+                        form.errors['new_password_confirm'] = ['Please confirm your new password.']
+                        still_valid = False
 
             if still_valid:
 
