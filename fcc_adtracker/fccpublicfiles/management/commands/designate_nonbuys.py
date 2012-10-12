106dd7713f32c3aa79f7d4f80a32499e5350dd2b
Jacob Fenton <jfenton@sunlightfoundation.com>
2012-10-12 17:41:14 -0400

    catch some errors

--- fcc_adtracker/fccpublicfiles/management/commands/designate_nonbuys.py
+++ fcc_adtracker/fccpublicfiles/management/commands/designate_nonbuys.py
@@ -5,7 +5,6 @@ from fccpublicfiles.models import PoliticalBuy
 
 invoice_res = [re.compile(r'invoice', re.I)]
 invalid_res = [re.compile(r'rebate', re.I), re.compile(r'nab\s+form', re.I), re.compile(r'rebate', re.I), re.compile(r'refund', re.I), re.compile(r'cancel', re.I), re.compile(r'rate card', re.I), re.compile(r'disclosure', re.I)]
-nabform_res = [re.compile(r'nab\s+form', re.I)]
 
 class Command(BaseCommand):
     help = "set invoices, etc"
@@ -16,33 +15,26 @@ class Command(BaseCommand):
         
         for buy in buys:
             for this_re in invoice_res:
-                if re.search(this_re, buy.related_FCC_file.raw_url):
-                    count += 1
-                    buy.is_invoice=True
-                    buy.save_no_update()
-                    
+                try:
+                    if re.search(this_re, buy.related_FCC_file.raw_url):
+                        count += 1
+                        buy.is_invoice=True
+                        buy.save_no_update()
+                except AttributeError:
+                    continue
         print "Found %s invoices" % (count)
         
         count = 0
         buys = PoliticalBuy.objects.exclude(is_invalid=True).exclude(is_FCC_doc=False).select_related('related_FCC_file')
         
-        rebate_count = 0
+        invalid_count = 0
         for buy in buys:
-            for this_re in invoice_res:
-                if re.search(this_re, buy.related_FCC_file.raw_url):
-                    rebate_count += 1
-                    buy.is_invalid=True
-                    buy.save_no_update()
-                    
-        print "Found %s rebates" % (count)
-        
-        
-        nab_count = 0
-        for buy in buys:
-            for this_re in nabform_res:
-                if re.search(this_re, buy.related_FCC_file.raw_url):
-                    nab_count += 1
-                    buy.is_invalid=True
-                    buy.save_no_update()
-                    
-        print "Found %s nab forms" % (nab_count)
+            for this_re in invalid_res:
+                try:
+                    if re.search(this_re, buy.related_FCC_file.raw_url):
+                        invalid_count += 1
+                        buy.is_invalid=True
+                        buy.save_no_update()
+                except AttributeError:
+                    continue
+        print "Found %s invalids" % (invalid_count)
