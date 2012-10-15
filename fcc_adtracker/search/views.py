2314501bb53f26922a2cc6cdb93cb5c3d409834a
Daniel Cloud <dcloud@sunlightfoundation.com>
2012-10-15 13:13:29 -0400

    Fixed date-range search so that pagination links don't throw away date criteria. This fixes and closes issue 100.

--- fcc_adtracker/search/views.py
+++ fcc_adtracker/search/views.py
@@ -1,6 +1,9 @@
 from haystack.views import FacetedSearchView
 
 
+DATE_FIELDS = ('start_date', 'end_date')
+
+
 class ImprovedFacetedSearchView(FacetedSearchView):
     def __name__(self):
         return "ImprovedFacetedSearchView"
@@ -10,6 +13,11 @@ class ImprovedFacetedSearchView(FacetedSearchView):
         facet_tuple = tuple([item.split(':') for item in self.request.GET.getlist('selected_facets')])
         facet_dict = dict([(item[0].split('_exact')[0], item[1]) for item in facet_tuple])
         extra['selected_facets'] = facet_dict
+        extra['date_filters'] = []
+        for key, value in self.request.GET.iteritems():
+            if key.startswith(DATE_FIELDS):
+                extra['date_filters'].append((key, value))
+        extra['date_filters'].sort()
         extra['sfapp_base_template'] = 'sfapp/base-full.html'
 
         return extra
