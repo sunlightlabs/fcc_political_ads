ac78df050a40e5d129fe38e0035b8c7c72275235
Jacob Fenton <jfenton@sunlightfoundation.com>
2012-10-22 13:32:02 -0400

    add pp adv name

--- fcc_adtracker/fccpublicfiles/management/commands/export_adbuys_to_S3.py
+++ fcc_adtracker/fccpublicfiles/management/commands/export_adbuys_to_S3.py
@@ -31,7 +31,7 @@ def all_ads_to_file():
     file_description="This file contains ads uploaded by volunteers and from the FCC's site available as of %s. Contract numbers and total ad buy amounts are from ProPublica's free the files project ( https://projects.propublica.org/free-the-files/ ) in rows where the 'data entry by' column is ProPublica." % most_recent_scrape.strftime("%Y-%m-%d %H:%m")
     print file_description
     file_name =  "%s/all_ad_buys.csv" % (CSV_EXPORT_DIR)
-    fields = ['id', 'station', 'file_upload_date', 'contract_start_date', 'contract_end_date', 'tv_market', 'tv_market_id', 'ad_type', 'fcc_folder', 'file_name', 'source_file_url', 'advertiser_name', 'is_invalid', 'is_invoice', 'total_spent_raw', 'num_spots_raw', 'contract_number', 'source', 'data entry by', 'notes']
+    fields = ['id', 'station', 'file_upload_date', 'contract_start_date', 'contract_end_date', 'tv_market', 'tv_market_id', 'ad_type', 'fcc_folder', 'file_name', 'source_file_url', 'advertiser_name', 'propublica_advertiser_name', 'is_invalid', 'is_invoice', 'total_spent_raw', 'num_spots_raw', 'contract_number', 'source', 'data entry by', 'notes']
     all_rows = PoliticalBuy.objects.all()
     file_rows = []
     for row in all_rows:
@@ -60,8 +60,12 @@ def all_ads_to_file():
         data_entry_by = ""
         if (row.using_pp_data):
             data_entry_by = "ProPublica"
+            
+        pp_adv_name = ''
+        if pp_data_ref:
+            pp_adv_name = pp_data_ref.v_committee
         
-        file_rows.append([row.pk, row.broadcaster_callsign, row.upload_time.strftime("%Y-%m-%d"), row.contract_start_date.strftime("%Y-%m-%d"), row.contract_end_date.strftime("%Y-%m-%d"), row.nielsen_dma, row.dma_id, row.candidate_type, row.fcc_folder_name, this_file_name, raw_url, advertiser_name, is_invalid, is_invoice, row.total_spent_raw, row.num_spots_raw, row.contract_number, row.doc_source(), data_entry_by, row.data_entry_notes])
+        file_rows.append([row.pk, row.broadcaster_callsign, row.upload_time.strftime("%Y-%m-%d"), row.contract_start_date.strftime("%Y-%m-%d"), row.contract_end_date.strftime("%Y-%m-%d"), row.nielsen_dma, row.dma_id, row.candidate_type, row.fcc_folder_name, this_file_name, raw_url, advertiser_name, pp_adv_name, is_invalid, is_invoice, row.total_spent_raw, row.num_spots_raw, row.contract_number, row.doc_source(), data_entry_by, row.data_entry_notes])
     
     write_csv_to_file(file_description, file_name, fields, file_rows)
     
