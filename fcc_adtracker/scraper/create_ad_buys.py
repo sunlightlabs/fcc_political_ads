00f1099a6982bd5ec146747010353d4049fbf01e
Jacob Fenton <jfenton@sunlightfoundation.com>
2014-07-31 16:32:43 -0400

    enforce limits on some problematic text fields

--- fcc_adtracker/scraper/create_ad_buys.py
+++ fcc_adtracker/scraper/create_ad_buys.py
@@ -19,16 +19,16 @@ def make_ad_buy_from_pdf_file(pdf_file_pk):
     pol_buy.is_FCC_doc= True
     pol_buy.related_FCC_file = pdf_file
     
-    pol_buy.candidate_type = pdf_file.candidate_type()
-    pol_buy.fcc_folder_name = pdf_file.raw_name_guess
-    pol_buy.nielsen_dma = pdf_file.nielsen_dma
+    pol_buy.candidate_type = pdf_file.candidate_type()[:31]
+    pol_buy.fcc_folder_name = pdf_file.raw_name_guess[:255]
+    pol_buy.nielsen_dma = pdf_file.nielsen_dma[:60]
     pol_buy.dma_id = pdf_file.dma_id
-    pol_buy.community_state =pdf_file.community_state
+    pol_buy.community_state =pdf_file.community_state[:7]
     pol_buy.upload_time = pdf_file.upload_time
     pol_buy.contract_start_date = pdf_file.upload_time
     pol_buy.contract_end_date = pdf_file.upload_time
     pol_buy.advertiser_display_name = str(pdf_file.raw_name_guess or "") + " - " + pdf_file.file_name()
-    pol_buy.broadcaster_callsign = pdf_file.callsign
+    pol_buy.broadcaster_callsign = pdf_file.callsign[:7]
     pol_buy.in_document_cloud = pdf_file.in_document_cloud
     
     pol_buy.save(auser)
